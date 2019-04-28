4b2274d1b13c8d3de57603a9b3ad241e3535c06d
artyomturkin <artm.turkin@live.com>
2019-04-28 17:18:55 +0600

    use ExecTasks to call next tasks

--- taskFormFunc.go
+++ taskFormFunc.go
@@ -2,8 +2,6 @@ package pipeline
 
 import (
 	"context"
-	"fmt"
-	"sync"
 )
 
 // TaskFromFunc creates a task from supplied func and continuation tasks
@@ -36,31 +34,5 @@ func (f *taskFromFunc) Execute(ctx context.Context, i interface{}) error {
 		return err
 	}
 
-	errs := []error{}
-	wg := &sync.WaitGroup{}
-	mu := &sync.Mutex{}
-
-	for _, t := range f.next {
-		wg.Add(1)
-
-		go func(t Task) {
-			defer wg.Done()
-
-			err := t.Execute(ctx, res)
-			if err != nil {
-				mu.Lock()
-				defer mu.Unlock()
-
-				errs = append(errs, err)
-			}
-		}(t)
-	}
-
-	wg.Wait()
-
-	if len(errs) > 0 {
-		return fmt.Errorf("Continuation tasks failed: %v", errs)
-	}
-
-	return nil
+	return ExecTasks(ctx, f.next, res)
 }
